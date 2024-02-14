25bd96f9a083a19b2cc62342eecd1a2b72ca2c78
PanitoMortal <aldoreyes2121@gmail.com>
2024-02-14 04:57:31 +0000

    cambios

--- threads/thread.h
+++ threads/thread.h
@@ -93,6 +93,8 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    uint64_t timer_sleep;
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -137,5 +139,6 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
-
+void insertar_en_lista_espera(int64_t);
+void remover_thread_durmiente(int64_t);
 #endif /* threads/thread.h */
